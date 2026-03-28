e086da6fc450ad48394a54685a0dd03d6806d298
Aliaksei <aleksey.vorobyov@gmail.com>
2026-03-28 15:02:24 +0100

    feat: finishQuestion

--- server/src/services/game.service.ts
+++ server/src/services/game.service.ts
@@ -1,22 +1,24 @@
+import { getPlayer } from "../store/player.store";
 import { Game, Question } from "../types";
 import { broadcastToSuccess } from "../utils/broadcast";
 
 const RESPONSE_TYPE = 'question';
+const BASE_POINTS = 1000;
 
 export function sendQuestion(game: Game): void {
-  const current: Question = game.questions[game.currentQuestion];
+  const currentQuestion: Question = game.questions[game.currentQuestion];
   game.questionStartTime = Date.now();
   game.playerAnswers = new Map();
   game.questionTimer = setTimeout(() => {
     finishQuestion(game);
-  }, current.timeLimitSec * 1000 );
+  }, currentQuestion.timeLimitSec * 1000 );
 
   broadcastToSuccess(game.id, RESPONSE_TYPE, {
       questionNumber: game.currentQuestion + 1,
       totalQuestions: game.questions.length,
-      text: current.text,
-      options: current.options,
-      timeLimitSec: current.timeLimitSec
+      text: currentQuestion.text,
+      options: currentQuestion.options,
+      timeLimitSec: currentQuestion.timeLimitSec
     })
 }
 
@@ -25,5 +27,43 @@ export function finishQuestion(game: Game): void {
     clearTimeout(game.questionTimer);
     game.questionTimer = undefined;
   }
-  // ToDo: actions after everybody answered
+  const currentQuestion: Question = game.questions[game.currentQuestion];
+  const playerResults: { name: string; answered: boolean; correct: boolean; pointsEarned: number; totalScore: number; }[] = [];
+  game.players.forEach(playerId => {
+    const player = getPlayer(playerId);
+    if (player) {
+      const answer = game.playerAnswers.get(playerId);
+      let points = 0;
+      let correct = false;
+      if (answer) {
+        correct = answer.answerIndex === currentQuestion.correctIndex;
+        if (correct) {
+          const timeLimit: number = currentQuestion.timeLimitSec;
+          const timeRemaining: number = timeLimit - (answer.timestamp - game.questionStartTime!) / 1000
+          points = Math.round(BASE_POINTS * Math.max(0, (timeRemaining / timeLimit)));
+        }
+      }
+      player.score += points;
+      playerResults.push({
+        "name": player.name,
+        "answered": Boolean(answer),
+        "correct": correct,
+        "pointsEarned": points,
+        "totalScore": player.score
+      })
+    }
+  });
+
+  broadcastToSuccess(game.id, 'question_result', {
+    questionIndex: game.currentQuestion,
+    correctIndex: currentQuestion.correctIndex,
+    playerResults
+  });
+
+  if (game.questions.length - 1 < game.currentQuestion) {
+    game.currentQuestion++;
+    sendQuestion(game);
+  } else {
+    // ToDo: finish game
+  }
 }
